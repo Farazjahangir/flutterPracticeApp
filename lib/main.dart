ac00c306260ab19c8be32583943ec148bdaa98a8
Farazjahangir <jahangirfaraz98@gmail.com>
2022-08-18 18:38:59 +0500

    added navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,20 @@
 import 'package:flutter/material.dart';
 
 import 'screens/login.dart';
+import 'screens/register.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  String? selectedRoute;
+
   @override
   Widget build(BuildContext ctx) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My App'),
-        ),
-        body: Login(),
-      ),
+      routes: {'Login': (ctx) => Login(), 'Register': (ctx) => Register()},
+      initialRoute: 'Login',
     );
   }
 }
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -18,65 +18,75 @@ class LoginState extends State<Login> {
     print(emailController);
   }
 
+  void navigateTo(route) {
+    Navigator.of(context).pushNamed(route);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Color(0xfffdfe4ea),
-      child: Container(
-        margin: const EdgeInsets.only(left: 13, right: 13),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              width: double.infinity,
-              margin: EdgeInsets.only(bottom: 18),
-              child: const Text(
-                "Login",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 40),
-              ),
-            ),
-            Container(
-              child: Form(
-                child: Column(
-                  children: [
-                    TextInput(
-                      controller: emailController,
-                      placeHolder: "Enter your email",
-                    ),
-                    TextInput(
-                      controller: passwordController,
-                      obscureText: true,
-                      placeHolder: 'Enter Your Password',
-                    ),
-                    Button('Login', OnPressed),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Container(
-                          child: TextButton(
-                            onPressed: () {},
-                            child: const Text('Register',
-                                style: TextStyle(
-                                    fontSize: 14, color: Colors.black)),
-                          ),
-                        ),
-                        Container(
-                            child: TextButton(
-                          onPressed: () {},
-                          child: const Text(
-                            'Forgot Password',
-                            style: TextStyle(fontSize: 14, color: Colors.black),
-                            textAlign: TextAlign.right,
-                          ),
-                        )),
-                      ],
-                    )
-                  ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My App'),
+      ),
+      body: Container(
+        color: Color(0xfffdfe4ea),
+        child: Container(
+          margin: const EdgeInsets.only(left: 13, right: 13),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: double.infinity,
+                margin: EdgeInsets.only(bottom: 18),
+                child: const Text(
+                  "Login",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 40),
                 ),
               ),
-            )
-          ],
+              Container(
+                child: Form(
+                  child: Column(
+                    children: [
+                      TextInput(
+                        controller: emailController,
+                        placeHolder: "Enter your email",
+                      ),
+                      TextInput(
+                        controller: passwordController,
+                        obscureText: true,
+                        placeHolder: 'Enter Your Password',
+                      ),
+                      Button('Login', OnPressed),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            child: TextButton(
+                              onPressed: () => navigateTo('Register'),
+                              child: const Text('Register',
+                                  style: TextStyle(
+                                      fontSize: 14, color: Colors.black)),
+                            ),
+                          ),
+                          Container(
+                              child: TextButton(
+                            onPressed: () {},
+                            child: const Text(
+                              'Forgot Password',
+                              style:
+                                  TextStyle(fontSize: 14, color: Colors.black),
+                              textAlign: TextAlign.right,
+                            ),
+                          )),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
